00cebb2e29d247387942b97ad538271b5b920973
Nikhil <nikhil.gundala@fedex.com>
2024-09-19 11:41:29 +0530

    Basic Setup

---
+++ src/main/java/com/uspace/UspaceBeApplication.java
@@ -0,0 +1,13 @@
+package com.uspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UspaceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UspaceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uspace/UspaceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UspaceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
